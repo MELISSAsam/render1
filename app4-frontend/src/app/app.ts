7116da44f5b43ef22145279c7cfaa9da36835515
eddyvlz001 <129920068+eddyvlz001@users.noreply.github.com>
2025-12-18 23:45:21 -0500

    Conectando componente saludo al app principal

--- app4-frontend/src/app/app.ts
+++ app4-frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { SaludoComponent } from './saludo/saludo'; // Verifica que la ruta sea correcta
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SaludoComponent], // Agrega SaludoComponent aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('app4-frontend');
-}
+export class AppComponent { }
